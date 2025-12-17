323895be25519c1f32aa9822b81b2fda35252f60
redgeography <128873224+redgeography@users.noreply.github.com>
2025-12-16 21:08:32 -0500

    Update threads.js

--- src/threads.js
+++ src/threads.js
@@ -2465,13 +2465,13 @@ Process.prototype.reportListItem = function (index, list) {
     }
     return value;
 };
-Process.prototype.reportReplacedInList = function (index,list,value) {
+Process.prototype.reportReplacedInList = function (value,index,list) {
 	this.assertType(list, "list");
 	let copy = new List(list.itemsArray().slice());
 	this.doReplaceInList(index,list,value);
 	return copy;
 }
-Process.prototype.reportInsertedInList = function (index,list) {
+Process.prototype.reportInsertedInList = function (value,index,list) {
 	this.assertType(list, "list");
 	let copy = new List(list.itemsArray().slice());
 	this.doInsertInList(index,list,value);
